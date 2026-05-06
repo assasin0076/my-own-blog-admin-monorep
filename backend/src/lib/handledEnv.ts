a6c3151b18eb09ec8496dcaed00f03fb4c11d8df
Viktor <ageneralov902@gmail.com>
2026-05-06 23:56:00 +0300

    feat: добавлена соль

--- backend/src/lib/handledEnv.ts
+++ backend/src/lib/handledEnv.ts
@@ -8,10 +8,12 @@ const envSchema = z.object({
     .min(1)
     .transform((value) => parseInt(value, 10)),
   FRONTEND_URL: z.string().min(1),
+  PASSWORD_SALT: z.string().min(1),
 });
 
 export const handledEnv = envSchema.parse({
   JWT_SECRET: process.env.JWT_SECRET,
   PORT: process.env.PORT,
   FRONTEND_URL: process.env.FRONTEND_URL,
+  PASSWORD_SALT: process.env.PASSWORD_SALT,
 });
--- backend/src/utils/getPasswordHash.ts
+++ backend/src/utils/getPasswordHash.ts
@@ -1,5 +1,6 @@
+import { handledEnv } from '@backend/lib/handledEnv';
 import crypto from 'crypto';
 
 export const getPasswordHash = (password: string) => {
-  return crypto.createHash('sha256').update(password).digest('hex');
+  return crypto.createHash('sha256').update(`${handledEnv.PASSWORD_SALT}${password}`).digest('hex');
 };
